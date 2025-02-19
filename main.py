3a5bb38bbfcfc35582a4f30bad810d8385c151c9
1a3orn <1a3orn@protonmail.com>
2025-02-19 10:12:53 -0500

    ASDA

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 from vllm import LLM, SamplingParams
 
 
-def load_llm(model_name):
-    llm = LLM(
-        model=model_name,
-        dtype="float16",
-    )
+def load_llm(model_path):
+    llm = LLM(model=model_path, dtype="float16")
     return llm
 
 
-prompts = [
-    "Hello, my name is",
-    "The president of the United States is",
-    "The capital of France is",
-    "The future of AI is",
-]
-sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
 
-llm = load_llm("agentica-org/DeepScale-R-1.5B-Preview")
+def main():
+    prompts = [
+        "Blork",
+    ]
+    sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
+
+    llm = load_llm("./agentica")
+
+    outputs = llm.generate(prompts, sampling_params)
+
+    for output in outputs:
+        prompt = output.prompt
+        generated_text = output.outputs[0].text
+        print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
+
+
+if __name__ == "__main__":
+    main()
